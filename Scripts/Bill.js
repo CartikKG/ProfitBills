8e1959e90501087a27b51a5e618c9314c14f68df
Kartik Gupta <kartikguptampi@gmail.com>
2022-10-25 22:16:02 +0530

    Bill css Modified

--- Scripts/Bill.js
+++ Scripts/Bill.js
@@ -1,11 +1,12 @@
+// mainallitems
 window.onload = function () {
   document.getElementById("download").addEventListener("click", () => {
     const invoice = this.document.getElementById("invoice");
     console.log(invoice);
     console.log(window);
     var opt = {
-      margin: 1,
-      filename: "myfile.pdf",
+      margin: 0.2,
+      filename: "Invoice.pdf",
       image: { type: "jpeg", quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: "in", format: "letter", orientation: "portrait" },

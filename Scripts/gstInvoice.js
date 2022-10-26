ea9b4a1fb69e231bf25afb095b5756c7e41864ff
Kartik Gupta <kartikguptampi@gmail.com>
2022-10-26 23:06:01 +0530

    invoice js almost done

--- Scripts/gstInvoice.js
+++ Scripts/gstInvoice.js
@@ -14,3 +14,47 @@ window.onload = function () {
     html2pdf().from(invoice).set(opt).save();
   });
 };
+
+document.getElementById("").innerHTML = `<li>
+<h5 class="my-2">Tibco Turang</h5>
+</li>
+<li>
+<span class="font-weight-semibold"
+  >Samantha Mutual funds Ltd</span
+>
+</li>
+<li>Gurung Street</li>
+<li>23 BB Lane</li>
+<li>Hong kong</li>
+<li>234 456 5678</li>
+<li><a href="#" data-abc="true">tibco@samantha.com</a></li>`;
+
+let tr = document.createElement("tr");
+tr.innerHTML = `<td>
+<h6 class="mb-0">Arts and design template</h6>
+<span class="text-muted"
+  >in reprehenderit in voluptate velit esse cillum dolore
+  eu fugiat nulla pariatur.Duis aute irure dolor in
+  reprehenderit</span
+>
+</td>
+<td>$120</td>
+<td>180</td>
+<td><span class="font-weight-semibold">$300</span></td>`;
+
+document.getElementById("").innerHTML = `<tr>
+<th class="text-left">Subtotal:</th>
+<td class="text-right">$1,090</td>
+</tr>
+<tr>
+<th class="text-left">
+  Tax: <span class="font-weight-normal">(25%)</span>
+</th>
+<td class="text-right">$27</td>
+</tr>
+<tr>
+<th class="text-left">Total:</th>
+<td class="text-right text-primary">
+  <h5 class="font-weight-semibold">$1,160</h5>
+</td>
+</tr>`;
--- Scripts/invoice.js
+++ Scripts/invoice.js
@@ -35,13 +35,47 @@ document.getElementById("maindiv").addEventListener("click", (e) => {
   `;
     document.querySelector("tbody").append(tr);
   } else if (e.target.id == "generate") {
-    // localStorage
-    let all = document.querySelectorAll("tbody>tr")[0];
-    console.log(all);
+    let all = document.querySelectorAll("tbody>tr");
+    let compnay = document.querySelectorAll("form>div>span")[0];
+    let c1 = document.querySelectorAll("form>div>span")[1];
+    function Bill_to(a, b, c, d, e, f) {
+      this.Name = a;
+      this.Mob = b;
+      this.GSTIN = c;
+      this.Address = d;
+      this.date = e;
+      this.No = f;
+    }
+    let Name = compnay.children[0].value;
+    let Mob = compnay.children[1].value;
+    let GSTIN = compnay.children[2].value;
+    let Address = compnay.children[4].value;
+    let date = c1.children[2].value;
+    let no = document.querySelector("#invoicecount").innerHTML;
+    let temp = new Bill_to(Name, Mob, GSTIN, Address, date, no);
+
+    console.log(temp);
+
+    function BillDetails(a, b, c, d, e, f) {
+      this.no = a;
+      this.des = b;
+      this.qty = c;
+      this.rate = d;
+      this.gst = e;
+      this.total = f;
+    }
+    for (let i = 1; i < all.length; i++) {
+      let sno = all[i].cells[0].innerHTML;
+      let des = all[i].cells[1].children[0].value;
+      let qty = all[i].cells[2].children[0].value;
+      let rate = all[i].cells[3].children[0].value;
+      let gst = all[i].cells[4].children[0].value;
+      let total = all[i].cells[5].children[0].value;
+      let temp = new BillDetails(sno, des, qty, rate, gst, total);
+      console.log(temp);
+    }
   } else if (e.target.id == "deleterow") {
-    // localStorage
     count--;
-    console.log("OKRE");
-    console.log(e.target.parentNode.remove());
+    e.target.parentNode.remove();
   }
 });
